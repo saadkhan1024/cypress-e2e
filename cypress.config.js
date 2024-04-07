a6afb3545cd56f334c0ed0d0e6436b339f9f1483
saadkhan1024 <saadkhan1024@gmail.com>
2024-04-07 13:46:39 +0530

    cypress.config updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: '',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  baseUrl: '',
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/reports',
